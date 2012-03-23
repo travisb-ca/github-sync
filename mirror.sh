1f7e1db741bad10f4ba7d0ba718184ba2030ca69
Travis Brown <travisb@travisbrown.ca>
2012-03-23 23:04:38 +0000

    Support repos not in the trunk/tags/branches layout
    git-svn-id: svn+ssh://svnrepo/projects/github-sync@4873 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- mirror.sh
+++ mirror.sh
@@ -3,14 +3,21 @@
 # Mirror a subversion subdirectory to github
 
 if [ -z $3 ]; then
-	echo "Usage: mirror.sh github_username name svnpath"
+	echo "Usage: mirror.sh github_username name svnpath [-T]"
+	echo "       Use -T when the svn location does not have trunk/tags/branches layout"
 	exit "1"
+fi
+
+if [ "$4" == "-T" ]; then
+	options = ""
+else
+	options = "--stdlayout"
 fi
 
 mkdir $2
 cd $2
 git init
-git svn init --stdlayout $3
+git svn init $options $3
 git svn fetch
 git gc
 git remote add origin git@githubmirror:$1/$2.git
