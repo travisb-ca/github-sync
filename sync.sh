1947d5a81a852306b4d0a52ac9c7e9e5d846f673
Travis Brown <travisb@travisbrown.ca>
2012-08-01 05:38:40 +0000

    Do a more complete check for having synced. Both fetch and rebase are necessary.
    git-svn-id: svn+ssh://svnrepo/projects/github-sync@5263 561c3618-0fe6-0310-b5d6-fc65c73fee30

--- sync.sh
+++ sync.sh
@@ -5,9 +5,19 @@
 for dir in */; do
 	echo "Syncing $dir"
 	cd $dir
-	git svn fetch | grep '$' &> /dev/null
+	needs_push=0
+	git svn fetch 2> /dev/null | grep '$' &> /dev/null
 	if [ $? == 0 ]; then
-		git push --tags origin master &> /dev/null
+		needs_push=1
+	fi
+	git svn rebase | grep "Current branch master is up to date" &>/dev/null
+	if [ $? == 1 ]; then 
+		needs_push=1
+	fi
+
+	if [ $needs_push == 1 ]; then
+		echo "Pushing."
+		#git push --tags origin master &> /dev/null
 	fi
 	cd ..
 done
